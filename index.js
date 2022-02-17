065011bc94ee608adea941c544bda62c714f2341
MDRahul47e <rahulahames@gmail.comm>
2022-02-17 15:11:22 +0600

    java id call

---
+++ index.js
@@ -0,0 +1,11 @@
+let income = document.getElementById("Income");
+let Food = document.getElementById("Food");
+let Rent = document.getElementById("Rent");
+let Clothes = document.getElementById("Clothes");
+let BTNcalculate = document.getElementById("Calculate-btn");
+let TotalExpense = document.getElementById("Total Expense");
+let Balance = document.getElementById("Balance");
+let saveInput = document.getElementById("save-input");
+let BTNsave = document.getElementById("save-btn");
+let SavingAmount = document.getElementById("Saving-Amount");
+let RemainingBalance = document.getElementById("Remaining-Balance");
