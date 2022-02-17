eff6b919c31a6177556a510b6d61a25d394d779b
MDRahul47e <rahulahames@gmail.comm>
2022-02-17 18:54:10 +0600

    recheck

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ let saveInput = document.getElementById("save-input");
 let BTNsave = document.getElementById("save-btn");
 let SavingAmount = document.getElementById("Saving-Amount");
 let RemainingBalance = document.getElementById("Remaining-Balance");
+let allInput = parseInt(document.querySelectorAll('input'));
 
 
-// --------------food rent clothes sum here---------------
-BTNcalculate.addEventListener('click', calculateHandler)
+// --------------food rent clothes and balance here---------------
+BTNcalculate.addEventListener('click', calculateHandler);
 function calculateHandler() {
     let mainIncome = parseInt(income.value);
     let costFood = parseInt(Food.value);
@@ -26,6 +27,27 @@ function calculateHandler() {
     let newBalance = mainIncome - TotalConst;
     Balance.innerText = newBalance;
     return newBalance;
+};
 
+// ---------------------save and % here -----------------------
+BTNsave.addEventListener("click", saveMoney);
+function saveMoney() {
+    let mainIncome = parseInt(income.value);
+    let numberPercent = parseInt(saveInput.value);
+    let parcentance = mainIncome / 100;
+    let finulSaveAmmount = parcentance * numberPercent;
+    if (finulSaveAmmount <= calculateHandler()) {
+        SavingAmount.innerText = finulSaveAmmount;
+        RemainingBalance.innerText = calculateHandler() - finulSaveAmmount;
+    }
+
+    else {
+        alert("sorry!! you don't have any sufficency blance")
+    };
 }
+
+
+
+
+
 
